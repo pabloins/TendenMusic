1b49d379970b07e3bc24a5b7c77487c047065593
p.nahuelpan02 <p.nahuelpan02@ufromail.cl>
2022-07-10 01:41:22 -0400

    navegabilidad primera parte

--- src/main/java/tendenmusic/tendenmusic/controller/CambioPantallaController.java
+++ src/main/java/tendenmusic/tendenmusic/controller/CambioPantallaController.java
@@ -1,16 +1,68 @@
 package tendenmusic.tendenmusic.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import tendenmusic.tendenmusic.Main;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CambioPantallaController implements Initializable {
 
+    @FXML
+    private Text panTitulo;
+
+    @FXML
+    private Button btnGenerarReporte;
+
+    @FXML
+    private Button btnContacto;
+
+    @FXML
+    private Button btnGenerarReporteConFiltro;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+    @FXML
+    private void handleClicks(ActionEvent event) throws IOException {
+        if(event.getSource() == btnGenerarReporte){
+            panTitulo.setText("Generar Reporte");
+            loadStage("VentanaGenerarReporte.fxml");
+        } else if (event.getSource() == btnGenerarReporteConFiltro) {
+            panTitulo.setText("Generar Reporte con Filtro");
+            loadStage("VentanaGenerarReporteFiltro.fxml");
+        } else if (event.getSource() == btnContacto) {
+            panTitulo.setText("Contacto");
+
+        }
+    }
+
+    private void loadStage(String fxml) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxml));
+        Parent root = loader.load();
+
+        Stage stage = new Stage();
+
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+
+    }
+
 }
